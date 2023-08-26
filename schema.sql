aeb7de02744e2a17e281c68f33cf829d1b7d73f0
Nathan Lai <naifen.04@gmail.com>
2023-08-26 16:01:40 +1000

    db

---
+++ schema.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS circle;
+DROP TABLE IF EXISTS task;
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE circle (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    owner_id INTEGER,
+    name TEXT,
+    FOREIGN KEY (owner_id) REFERENCES user (id)
+);
+
+CREATE TABLE belongsTo (
+    user_id INTEGER NOT NULL,
+    circle_id INTEGER NOT NULL,
+    admin INTEGER,
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (circle_id) REFERENCES circle (id),
+    PRIMARY KEY (user_id, circle_id)
+);
+
+CREATE TABLE task (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    circle_id INTEGER NOT NULL,
+    completed INTEGER,
+    name TEXT,
+    description TEXT
+);
