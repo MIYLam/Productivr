194f750253ff20d76d07b8bbd3879634995f1c5a
RiceNLamb <mlam7947@uni.sydney.edu.au>
2023-08-26 22:16:08 +1000

    added buttons being able to self delete

---
+++ static/taskinteractions.js
@@ -0,0 +1,20 @@
+
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    var deletionButton = document.querySelectorAll(".deletion");
+    
+    // Loop through each button and add an event listener
+    deletionButton.forEach(function(button) {
+        button.addEventListener("click", function() {
+            // Find the closest parent element with an ID
+            var parentDiv = button.closest("[id]");
+            
+            if (parentDiv) {
+                var parentId = parentDiv.id;
+                const parent_element = document.getElementById(parentId);
+                parent_element.remove();
+            }
+        });
+    });
+});
