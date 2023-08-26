c9db569ee0035902874b3b67b0efa4d35bb9819f
Nathan Lai <naifen.04@gmail.com>
2023-08-26 19:19:08 +1000

    added to sql helper

--- sql_helper.py
+++ sql_helper.py
@@ -18,6 +18,72 @@ def execute_commands(conn, cmd):
     except Exception as e:
         print(e)
 
+def retrieve_table(conn: sqlite3.Connection, cmd: str):
+    try:
+        return pd.read_sql(sql = cmd, con = conn)
+    except Exception as e:
+        print(e)
+
+def add_user(conn: sqlite3.Connection, username: str):
+    try:
+        c = conn.cursor()
+        c.execute(f"""INSERT INTO user("name") VALUES('{username}');""")
+        conn.commit()
+    except Exception as e:
+        print(e)
+
+def add_circle(conn: sqlite3.Connection, circlename: str, owner_id: int):
+    try:
+        c = conn.cursor()
+        c.execute(f"""INSERT INTO cirle(name, owner_id) VALUES('{circlename}', {owner_id});""")
+        c.execute(f"""INSERT INTO belongsTo(user_id, circle_id, admin) VALUES({owner_id}, (SELECT MAX(id) from circle), TRUE);""")
+        conn.commit()
+    except Exception as e:
+        print(e)
+
+def user_join_circle(conn: sqlite3.Connection, user_id: int, circle_id: int):
+    try:
+        c = conn.cursor()
+        c.execute(f"""INSERT INTO belongsTo(user_id, circle_id, admin) VALUES({user_id}, {circle_id}, FALSE);""")
+        conn.commit()
+    except Exception as e:
+        print(e)
+
+def add_task(conn: sqlite3.Connection, user_id: int, circle_id: int, name: str, description: str):
+    try:
+        c = conn.cursor()
+        c.execute(f"""INSERT INTO belongsTo(user_id, circle_id, completed, name, description) VALUES({user_id}, {circle_id}, FALSE, '{name}', '{description});'""")
+        conn.commit()
+    except Exception as e:
+        print(e)
+
+def get_user_tasks(conn: sqlite3.Connection, user_id: int, circle_id = -1: int):
+    try:
+        c = conn.cursor()
+        if circle_id == -1:
+            c.execute(f"""SELECT * FROM task WHERE user_id = {user_id} GROUP BY circle_id;""")
+        else:
+            c.execute(f"""SELECT * FROM task WHERE user_id = {user_id} AND circle_id = {circle_id};""")
+        records = c.fetchall()
+        return records
+    except Exception as e:
+        print(e)
+
+def get_group_users(conn: sqlite3.Connection, circle_id: int):
+    try:
+        c = conn.cursor()
+        c.execute(f"""SELECT * FROM user WHERE id IN (SELECT user_id FROM belongsTo WHERE circle_id = {circle_id});""")
+        records = c.fetchall()
+        return records
+    except Exception as e:
+        print(e)
+
+def complete_task(conn: sqlite3.Connection, task_id: int):
+    try:
+        c = conn.cursor()
+        c.execute(f"""UPDATE task SET completed = TRUE WHERE id = {task_id};""")
+    except Exception as e:
+        print(e)
 
 # conn = get_conn_object("./data.db")
 
