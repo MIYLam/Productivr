7a0d2221ba40c2f8a0d4720c520877f8fc2c6245
Rueien <tanrueien@gmail.com>
2023-08-26 15:51:11 +1000

    added functionality to classes

--- Task.py
+++ Task.py
@@ -1,11 +1,24 @@
-class Task:
-    
+class Task:  
     def __init__(self, title, description, user_id, group_id):
         self.title = title
         self.description = description
         self.is_completed = False
         self.user = user_id
         self.group = group_id
+        self.taskID = self.generateID()
+        self.updateDatabase()
+    
+    def updateDatabase(self):
+        """
+        put the data into database
+        """
+        pass
+    
+    def generateID(self):
+        """
+        Pull from database, and count number of rows + 1 and assign it as the ID
+        """
+        return 1
     
     def mark_as_completed(self):
         self.is_completed = True
